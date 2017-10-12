8a161c2f04ce8b4cf5105a898571402bdeb871e1
Serhii Komarychev <komarichev@gmail.com>
2017-10-12 21:53:15 +0300

    add quotes

--- js/index.js
+++ js/index.js
@@ -2,8 +2,28 @@ const continueBtn = document.querySelector('#continue');
 const returnBtn = document.querySelector('#return');
 const randomBtn = document.querySelector('#random');
 const app = document.querySelector('#app');
-const quote = document.querySelector('.quote');
+const quote = document.querySelector('.quote span');
+
+
+const quotes = [
+    {
+        text  : "В жизни должна быть любовь - одна великая любовь за всю жизнь, это оправдывает беспричинные приступы отчаяния, которым мы подвержены.",
+        author: "Альберт Камю"
+    },
+    {
+        text  : "Хочу хронического здоровья, прогрессирующего счастья, рецидивирующего успеха, гипертонической зарплаты, и вечно беременного кошелька без угрозы выкидыша!",
+        author: "Альберт Камю"
+    },  
+    {
+        text  : "- Мам, скоро родительское собрание в школе, и я хотел бы анонсировать готовящиеся информационные атаки на меня.",
+        author: "Альберт Камю"
+    }
+];
+document.addEventListener("DOMContentLoaded", () => {
+    quote.innerHTML = `${quotes[0].text} <br> ${quotes[0].author}`;
+});
+
 
 continueBtn.addEventListener('click', () => {
-    quote.innerHTML = "Hello" + Math.random()*10;
+    quote.innerHTML = `${quotes[0].text}`;
 });
